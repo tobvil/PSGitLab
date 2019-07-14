c9809995ce8f4c257f11a17b1065bfe61fced707
Tobias Vilhelmsen <tobias.vilhelmsen@sentia.dk>
2019-07-14 22:57:26 +0200

    Adds help

--- PSGitLab/Public/Branches/Get-GitLabBranch.ps1
+++ PSGitLab/Public/Branches/Get-GitLabBranch.ps1
@@ -1,17 +1,33 @@
+<#
+.Synopsis
+    Get-GitLabBranch - Find branches on a specific project
+.DESCRIPTION
+    Function for finding branches on a specific project. Can return all branches, specific branch, or search for branches by name.
+.EXAMPLE
+    Get-GitLabBranch -ProjectId 64 -All
+.EXAMPLE
+    Get-GitLabBranch -ProjectId 64 -Branch Master
+.EXAMPLE
+    Get-GitLabBranch -ProjectId 64 -Search Test
+#>
 Function Get-GitLabBranch {
     [cmdletbinding(DefaultParameterSetName = 'All')]
     [OutputType('GitLab.Branch')]
     param(
-        
+
+        #Id of the project
         [Parameter(Mandatory = $true)]
         [string]$ProjectId,
 
+        #Will return all branches associated to project
         [Parameter(ParameterSetName = 'All')]
         [switch]$All,
 
+        #Specify branch name to return specific branch
         [Parameter(ParameterSetName = 'Branch')]
         [string]$Branch,
 
+        #Search for branch
         [Parameter(ParameterSetName = 'Search')]
         [string]$Search
     )
